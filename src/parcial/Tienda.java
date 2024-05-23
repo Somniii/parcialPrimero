10b87b29dfdd4916d6451620bce7ad6d6ffb28ba
Ian <Somnii006@gmail.com>
2024-05-23 16:53:58 -0300

    enviando buscar articulo

--- src/parcial/Tienda.java
+++ src/parcial/Tienda.java
@@ -49,4 +49,24 @@ public class Tienda extends Empresa{
     public void setTransaccion(Transacciones[] transaccion) {
         this.transaccion = transaccion;
     }
+    public void agregarArticulo(Articulo articulo){
+        System.out.println("Que articulo desea agregar\n");
+        for(Articulo aux: inventario){
+
+        }
+    }
+    public Articulo buscarArticulo(String nombreBuscar){
+        if(inventario != null){
+            for(Articulo aux: inventario){
+                if(aux.getNombre()==nombreBuscar){
+                    System.out.println("Se econtro el articulo");
+                    System.out.println("ID:"+aux.getId()+"\nNOMBRE:"+aux.getNombre()+"\nPRECIO:"+aux.getPrecio());
+                }
+            }
+        }
+        else{
+            System.out.println("El inventario esta vacio");
+        }
+
+    }
 }
